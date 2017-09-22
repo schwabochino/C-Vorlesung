b96de4c620bec1e91868e324eb7d3f4c0306f3cb
Philipp Schwarberg <philipp.schwarberg@outlook.de>
2017-09-22 18:55:27 +0200

    Aufgabe 3.07 angefangen

---
+++ 3_07 KleinsteZahl/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float zahl1 =0, zahl2 =0, zahl3=0;
+    printf("Geben Sie die erste Zahl ein:");
+    scanf("%d", &zahl1);
+    fflush(stdin);
+    printf("Geben Sie die zweite Zahl ein:");
+    scanf("%d", &zahl2);
+    fflush(stdin);
+     printf("Geben Sie die dritte Zahl ein:");
+    scanf("%d", &zahl3);
+    fflush(stdin);
+    if((zahl1 < zahl2) && (zahl1 < zahl3)){
+        printf("%d", zahl1);
+        }else if
+    return 0;
+}
