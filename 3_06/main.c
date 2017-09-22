1e26af34c75296cdcb773c0497a3d836d18059c8
Philipp Schwarberg <philipp.schwarberg@outlook.de>
2017-09-22 18:43:41 +0200

    Aufgabe 3.06 erledigt

--- 3_06/main.c
+++ 3_06/main.c
@@ -4,8 +4,28 @@
 int main()
 {
     int punkte=0;
+    char note[15];
     printf("Punktzahl: ");
     scanf("%d", &punkte);
     fflush(stdin);
-printf("Punkte: ");
-}
+
+    if(punkte == 10){
+        printf("Sehr gut!");
+    }else if(punkte == 9){
+            printf("gut");
+        }else if(punkte == 8){
+            printf("befriedigend");
+        }else if(punkte == 7){
+            printf("ausreichend");
+        }else if(punkte == 7){
+            printf("mangelhaft");
+        }else if(punkte < 6 && punkte >= 1){
+            printf("ungenuegend");
+            }else{
+                printf("Fehler bei der Eingabe!");
+            }
+
+    }
+
+
+
