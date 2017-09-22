5886aae03ab3626c3761715fb7b3fe6dfef07e1f
Philipp Schwarberg <philipp.schwarberg@outlook.de>
2017-09-22 17:40:34 +0200

    Überarbeitet

--- 3_06/main.c
+++ 3_06/main.c
@@ -7,30 +7,5 @@ int main()
     printf("Punktzahl: ");
     scanf("%d", &punkte);
     fflush(stdin);
-
-    if (punkte >= 10);
-    {
-        printf("Note: Sehr Gut!\n");
-
-    } else if(punkte ==  9)
-    {
-        printf("Note: Gut!\n");
-
-    } else if(punkte ==  8)
-    {
-        printf("Note: befriedigend!\n");
-
-    }else if(punkte ==  7)
-    {
-        printf("Note: ausreichend!\n");
-
-    }else if(punkte ==  6)
-    {
-        printf("Note: mangelhaft!\n");
-
-    }else if(punkte < 6)
-    {
-        printf("Note: ungenügend\n");
-    }
-    return 0;
+printf("Punkte: ");
 }
