0ada7d309cceec8f3f2d7b6c74d6dd7cef49a888
schwabochino <philipp.schwarberg@outlook.de>
2017-09-24 13:39:18 +0200

    3.12

---
+++ 3_20Rechteckausgeben/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+3.20 Geben Sie ein Rechteck 10*10 Zeichen mit * aus
+
+int main()
+{
+    int y=0, i=1;
+
+    while(i<100)
+    {
+        y = i%10;
+        if(y==0){
+            printf("\n");
+        }else
+    printf("*");
+    i++;
+    }
+    return 0;
+}
